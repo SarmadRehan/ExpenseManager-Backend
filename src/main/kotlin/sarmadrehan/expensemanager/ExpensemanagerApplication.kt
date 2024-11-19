78bc79dbbcae21940c4280344394399ad56a6997
Sarmad Rehan <sarmadrehan91@gmail.com>
2024-11-19 13:25:34 +0500

    Initial Commit

---
+++ src/main/kotlin/sarmadrehan/expensemanager/ExpensemanagerApplication.kt
@@ -0,0 +1,11 @@
+package sarmadrehan.expensemanager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ExpensemanagerApplication
+
+fun main(args: Array<String>) {
+	runApplication<ExpensemanagerApplication>(*args)
+}
